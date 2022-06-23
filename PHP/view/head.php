13766d7cba162cef9e1c4b31dfd51c7123137dcd
Matheus Paim <65717016+matheuspaiim@users.noreply.github.com>
2022-06-23 15:20:41 -0300

    css and js

--- PHP/view/head.php
+++ PHP/view/head.php
@@ -8,6 +8,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://matheuspaiim.github.io/Kairos.Working/CSS/main.css">
+    <script src="https://matheuspaiim.github.io/Kairos.Working/JS/main.js"></script>
     <title>KairosWorking</title>
     <style>
         .kw-menu {
